01bcb01411cb09880c9b42a7ef67d77b69d3e9dc
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2021-09-18 11:30:45 -0400

    queue.js: implemented a queue

---
+++ src/commands/queue.js
@@ -0,0 +1,40 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+let {queue} = require("./../data/musicQueue");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('queue')
+		.setDescription('Adds a song to the queue')
+        .addStringOption(option => option.setName('song').setDescription('Enter a song name')),
+	async execute(interaction) {
+        if (interaction.options.getString("song") === null) {
+            const embed = new MessageEmbed();
+            embed.setColor("AQUA");
+            embed.setTitle("Your Queue");
+            let foundChannel = false;
+            for (const channel of queue) {
+                if (channel.channelId === interaction.guild_id) {
+                    foundChannel = true;
+                    for (const song of channel.channelQueue) {
+                        embed.addField(`${song.name}`, "song description...?");
+                    }
+                }
+            }
+            await interaction.reply({ embeds: [embed] });
+        } else {
+            // console.log(interaction.guild_id);
+            let foundChannel = false;
+            for (const channel of queue) {
+                if (channel.channelId === interaction.guild_id) {
+                    channel.channelQueue.push({name: interaction.options.getString("song")})
+                    foundChannel = true;
+                }
+            }
+            if (!foundChannel) {
+                queue.push({channelId: interaction.guild_id, channelQueue: [{name: interaction.options.getString("song")}]})
+            }
+            await interaction.reply(`${interaction.options.getString("song")} has been added to the queue!`);
+        }
+	},
+};
---
+++ src/data/musicQueue.js
@@ -0,0 +1,12 @@
+
+let queue = [];
+/**
+ * queue structure:
+ * {
+ *  voice channel id,
+ *  channelQueue: {
+ *      array = [ {song name, mp3} ]
+ *  }
+ * }
+ */
+exports.queue = queue;
