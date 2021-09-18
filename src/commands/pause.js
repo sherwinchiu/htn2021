181518a554a7306cab1feb819e0ada5fd6db12d4
sashabarazandeh <57276791+sashabarazandeh@users.noreply.github.com>
2021-09-18 17:03:40 -0400

    sasha work

--- src/commands/pause.js
+++ src/commands/pause.js
@@ -1,9 +1,22 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const ytdl = require("ytdl-core");
+const ytSearch = require("yt-search");
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('pause')
-		.setDescription(' '),
+		.setDescription('/pause (song name/url)'),
 	async execute(interaction, client) {
-	},
+		if (client.voice.adapters.length > 0) {
+			interaction.reply("Being used somewhere else");
+		} 
+		else {
+			 if (interaction.member.voice.channel) {
+				
+				await interaction.reply('**Pausing** (song name/song url)');
+			} else {
+				await interaction.reply("Please join a voice channel first");
+			}
+		},
 };
--- src/commands/skip.js
+++ src/commands/skip.js
@@ -1,9 +1,16 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+onst { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const ytdl = require("ytdl-core");
+const ytSearch = require("yt-search");
+let {queue} = require("./../data/musicQueue");
 
+
+const dispatcher = queue
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('skip')
-		.setDescription(' '),
+		.setDescription('/skip (skipping song name/url), skips current song and moves to next song in queue. Fails if there is no queue'),
 	async execute(interaction, client) {
+		
 	},
 };
